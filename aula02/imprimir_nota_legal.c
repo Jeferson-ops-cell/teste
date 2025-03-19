880dc39fe354eaf9595138b3894d2f6fc0a219a5
unknown <2512130067@ACADEMICO.IESB.BR>
2025-03-19 09:58:42 -0300

    a

--- aula02/imprimir_nota_legal.c
+++ aula02/imprimir_nota_legal.c
@@ -5,11 +5,11 @@ int main() {
     printf("      NOTA LEGAL    \n");
     printf("--------------------\n");
     printf("Item  Qde Prc  Valor\n");
-    printf("%s %i %f %f\n", "Caneta Azul",  2, 2.0, 4.0);
-    printf("%s %i %f %f\n", "Borracha", 1,  5.0,  5.0);
-    printf("%s %i %f %f\n", "Resma de Papel", 1,  12.0,  12.0);
+    printf("%-10s %03i %7.2f %7.2f\n", "Caneta Azul",  2, 2.0, 4.0);
+    printf("%-10s %03i %7.2f %7.2f\n", "Borracha", 1,  5.0,  5.0);
+    printf("%-10s %03i %7.2f %7.2f\n", "Resma de Papel", 1,  12.0,  12.0);
     printf("---------------------\n");
-    printf("TOTAL..............: R$ %f\n", 21.0);
+    printf("TOTAL..............: R$ %7.2f\n", 21.0);
 
     return 0;
 }
