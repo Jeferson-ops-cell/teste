68e83f874c965e14de93e71dff35476505bb6469
unknown <2512130067@ACADEMICO.IESB.BR>
2025-03-19 11:01:00 -0300

    a

---
+++ praticas/pratica02/imprima_fibonacci.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("11");
+    printf("121");
+    printf("12321");
+    printf("1 2 3 5 3 2 1");
+    printf("1 2 3 5 8 5 3 2 1");
+
+  return 0;
+}
---
+++ praticas/pratica02/imrpima_seu_nome.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Jeferson dos Santos Silva\n");
+
+
+  return 0;
+}
