6ada41da9f7b899a4025115f0d70b343337ab3a0
unknown <2512130067@ACADEMICO.IESB.BR>
2025-03-26 10:49:42 -0300

    código das aulas 3 e 4

---
+++ aula03/modificadores_de_tipo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char idade = 123;
+    unsigned char idade_positiva = 255; // 0 a 255
+
+    int populacao_china = 400000000; // -2bi a 2bi
+    unsigned int populacao_india = 300000000; // 0 a 4bi 
+
+    short int inteiro_2bytes = 3200; // -32mil a 32mil
+    unsigned short int inteiro_2bytes_positivo = 64; //0 a 64mil
+    long int inteiro_8bytes = 90000000000000; //-91lhoes a + 9lhoea
+    unsigned long int interos_8bytes_positivo = 10000000000000; // 0 a 10 lhoes
+
+    long double duplo_16bytes = 15165165165561654411.5198494894984L;
+
+    return 0;
+}
